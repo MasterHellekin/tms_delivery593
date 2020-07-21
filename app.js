8bd783a88fb19b5d628a7001e8a500330c9fa746
Hellekin <clsjs94@hotmail.com>
2020-07-21 17:09:52 -0500

    V 1.0.2 build complete

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ app.use("/api/pia", piaRoute);
 app.use("/api/auth", authRoute);
 
 // Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
